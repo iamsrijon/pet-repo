242758b46909ba0aabc4c1089b1c0a326e123e1d
Shuvradeb Barman Srijon <rusrijon@yahoo.com>
2022-09-27 11:31:03 -0500

    Refactored service interfaces, added CrudService
    closes #11

--- pet-clinic-data/src/main/java/com/srijon/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/srijon/petclinic/services/OwnerService.java
@@ -2,11 +2,6 @@ package com.srijon.petclinic.services;
 
 import com.srijon.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-    Owner findById(Long id);
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/srijon/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/srijon/petclinic/services/PetService.java
@@ -3,10 +3,6 @@ package com.srijon.petclinic.services;
 import com.srijon.petclinic.model.Owner;
 import com.srijon.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
